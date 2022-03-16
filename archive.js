eabe0eb735faca6ad3a0b0ec42c2eb92119499ff
StephanyCruzV <stephanycruzv@outlook.com>
2022-03-16 17:54:53 -0600

    Comentario y corregir nombre

--- archive.js
+++ archive.js
@@ -1,12 +1,13 @@
+
 Vue.createApp({
   data() {
     return {
       information: {
-        name: "==== Stephany Judith CRuz Vázquez ====",
+        name: "===== Stephany Judith Cruz Vázquez =====",
         id: "Matrícula: [A00824927]"
       },
       interests: ["Puzzles", "Sudoku", "Basketball", "Reading", "Sleep"],
-      picsumImage: "https://picsum.photos/200/300"
+      picsumImage: "https://picsum.photos/200/300" // Imagen aleatoria
     };
   }
 }).mount("#app");
